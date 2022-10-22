75a6cf12a9c3d2ed7453e6a97676d1fa20a7d7cb
BitNewPlayer <96981581+HarjeetPadda@users.noreply.github.com>
2022-10-22 17:40:07 +1100

    Update LeapYear1-1.js

--- Assignment1/LeapYear1-1.js
+++ Assignment1/LeapYear1-1.js
@@ -1,17 +1,9 @@
 // Leap Year
-const year = 2004
+const year = 2020
 
 //Check if not evenly divisible by 100 than check if evenly divisible by 4 return true plus year if not move to else if block and check if divisible by 100 and 400  return true plus year 
 if (year % 100 != 0) {
-    if (year % 4 === 0) {
-        console.log("Leap Year: " + year);
-    } else {
-        console.log("Not leap year: " + year);
-    }
+    (year % 4 === 0) ? console.log("Leap Year: " + year) : console.log("Not leap year: " + year);
 }else if (year % 100 === 0) {
-    if (year % 400 === 0) { 
-        console.log("Leap Year: " + year);
-    }else {
-        console.log("Not Leap Year: " + year);
-    }
+    (year % 400 === 0) ? console.log("Leap Year: " + year) : console.log("Not Leap Year: " + year);
 }
